492665e5488f3a8823b8544f2ebeb827182ab533
ylamsiah <110131779+YuSuFLams@users.noreply.github.com>
2022-11-22 16:10:54 +0100

    Dragon (* ~ *)

--- ft_uputnbr.c
+++ ft_uputnbr.c
@@ -17,11 +17,11 @@ void	ft_uputnbr(unsigned int n, int *d)
 	long	nb;
 
 	nb = n;
-	if (n < 10)
+	if (nb < 10)
 		ft_putchar(n + 48, d);
 	else
 	{
-		ft_uputnbr(n / 10, d);
-		ft_uputnbr(n % 10, d);
+		ft_uputnbr(nb / 10, d);
+		ft_uputnbr(nb % 10, d);
 	}
 }
